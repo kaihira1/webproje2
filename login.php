7d744c3e2d4e7ea19873a7ed86d400c56bddaac5
kaihira1 <yunusemretuncer111@gmail.com>
2025-05-17 00:08:24 +0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+
+        </title>
+        <link rel="stylesheet"type="text/css"href="logstyle.css">
+    </head>
+    <body>
+        <form action="index.php" method="post"><h2>LOGİN</h2>
+        <label for="">User Name</label>
+        <input type="text"name="uname" placeholder="User Name">
+        <label for="">Password</label>
+        <input type="password"name="uname" placeholder="Password">
+
+        <button type="submit">Login</button>
+    
+    </form>
+    </body>
+</html>
